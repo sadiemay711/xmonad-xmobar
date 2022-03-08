9645a7bb04054fb2989180f029d199e4cb4ffd23
Sadie May <77846895+sadiemay711@users.noreply.github.com>
2022-03-07 20:49:45 -0600

    Add files via upload

---
+++ xmobarrc.hs
@@ -0,0 +1,59 @@
+Config {
+       font = "xos4 Terminus:pixelsize=17:antialias=true"
+       , additionalFonts = [ "xft:FontAwesome:size=11" ]
+       , allDesktops = True
+       , bgColor = "#282c34"
+       , fgColor = "#bbc2cf"
+       , position = TopW L 100
+       , commands = [ Run Cpu [ "--template", "CPU: <total>%"
+                              , "--Low","3"
+                              , "--High","50"
+                              , "--low","#bbc2cf"
+                              , "--normal","#bbc2cf"
+                              , "--high","#fb4934"] 50
+
+                    , Run Memory ["-t","MEM: <usedratio>%"
+                                 ,"-H","80"
+                                 ,"-L","10"
+                                 ,"-l","#bbc2cf"
+                                 ,"-n","#bbc2cf"
+                                 ,"-h","#fb4934"] 50
+
+                    , Run Date "%a %b %_d %H:%M:%S" "date" 10 
+                    , Run DynNetwork ["-t","Down: <rx>, Up: <tx>"
+                                     ,"-H","200"
+                                     ,"-L","10"
+                                     ,"-h","#bbc2cf"
+                                     ,"-l","#bbc2cf"
+                                     ,"-n","#bbc2cf"] 50
+
+                    , Run CoreTemp ["-t", "Temp: <core0>°"
+                                   , "-L", "30"
+                                   , "-H", "75"
+                                   , "-l", "lightblue"
+                                   , "-n", "#bbc2cf"
+                                   , "-h", "#aa4450"] 50
+
+                    -- battery monitor
+                    , Run BatteryP       [ "BAT0" ]
+                                         [ "--template" , "BAT: <acstatus>"
+                                         , "--Low"      , "10"        -- units: %
+                                         , "--High"     , "80"        -- units: %
+                                         , "--low"      , "#fb4934" -- #ff5555
+                                         , "--normal"   , "#bbc2cf"
+                                         , "--high"     , "#98be65"
+
+                                         , "--" -- battery specific options
+                                                   -- discharging status
+                                                   , "-o"   , "<left>% (<timeleft>)"
+                                                   -- AC "on" status
+                                                   , "-O"   , "<left>% (<fc=#98be65>Charging</fc>)" -- 50fa7b
+                                                   -- charged status
+                                                   , "-i"   , "<fc=#98be65>Charged</fc>"
+                                         ] 50
+                    , Run StdinReader
+                    ]
+       , sepChar = "%"
+       , alignSep = "}{"
+       , template = "%StdinReader% } %date% { %cpu% | %coretemp% | %memory% | %battery% | %dynnetwork% |"   -- #69DFFA
+       }
